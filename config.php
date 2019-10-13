620b027eba728ed9614251e7f72bd38f1b7a34f2
Luke Morgan <morgan.luke94@gmail.com>
2019-10-13 12:00:35 +0100

    config.php cleanup

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ const headers = ['TRN-Api-Key' => TRACKER_API_KEY ];
 
 //instantiate guzzle class
      $client = new Client([
-        'headers' => ['Content-Type' => 'application/json', 'Accept' => 'application/json', 'Access-Control-Allow-Origin' => $_SERVER['HTTP_ORIGIN'], 'Access-Control-Allow-Headers' => 'X-Requested-With, Origin'
+        'headers' => ['Content-Type' => 'application/json', 'Accept' => 'application/json'
     ],
         // Base URI is used with relative requests
         'base_uri' => TRACKER_API_URL,
